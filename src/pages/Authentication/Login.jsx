a2da7d4da266965dd8fcf8ea3e43dd0649421966
Omar Issa <omarissa@oryxdentalsoftware.com>
2023-06-24 14:00:06 +0300

    added apis and loader

--- src/pages/Authentication/Login.jsx
+++ src/pages/Authentication/Login.jsx
@@ -1,10 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {MDBContainer, MDBCol, MDBRow, MDBBtn, MDBInput } from 'mdb-react-ui-kit';
 import './Login.css';
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setLogin } from "../../state";
+import { SERVER_URL } from "../../constants/constants";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Loader from "../Loader";
 
 function Login() {
 
+  const [formData, setFormData] = useState({});
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const submitToastId = React.useRef(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    axios.post(`${SERVER_URL}/api/v1/auth/login`, formData).then((res) => {
+        setIsLoading(false);
+        dispatch(
+          setLogin({
+            user: res.data.user,
+            token: res.data.token,
+          })
+        );
+        navigate("/home");
+        if (!toast.isActive(submitToastId.current)) {
+        toast.success("Login successful", {
+          toastId: submitToastId.current,
+          position: "bottom-right",
+          autoClose: 4000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          draggable: false,
+          progress: undefined,
+          theme: "dark",
+        });
+      };
+      }
+    ).catch((error) => {
+      if (!toast.isActive(submitToastId)) {
+        toast.error("Login failed", {
+          toastId: submitToastId.current,
+          position: "bottom-right",
+          autoClose: 4000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          draggable: false,
+          progress: undefined,
+          theme: "dark",
+        });
+      }
+    });
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  if (isLoading) {
+    return (
+      <Loader />
+    )
+  }
+
   return (
+    <>
     <MDBContainer fluid className="p-3 container">
 
 
@@ -30,11 +97,11 @@ function Login() {
 
             <div className='input-wrapper'>
 
-            <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg"/>
-            <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg"/>
+            <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' name='email' type='email' size="lg" onChange={handleChange}/>
+            <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' name='password' type='password' size="lg" onChange={handleChange}/>
 
             <div className='text-center text-md-center mt-4 pt-2'>
-                <MDBBtn className="mb-0 px-5" size='lg'>Login</MDBBtn>
+                <MDBBtn onClick={submitForm} className="mb-0 px-5" size='lg'>Login</MDBBtn>
                 <p className="small fw-bold mt-2 pt-1 mb-2">Don't have an account? <a href="/signup" className="link-primary links">Register</a></p>
             </div>
 
@@ -53,6 +120,17 @@ function Login() {
       </div>
 
     </MDBContainer>
+          <ToastContainer
+          position="bottom-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          draggable={false}
+          theme="light"
+        />
+        </>
   );
 }
 
--- src/pages/Authentication/Signup.jsx
+++ src/pages/Authentication/Signup.jsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { SERVER_URL } from "../../constants/constants";
+
 import {
   MDBBtn,
   MDBContainer,
@@ -11,6 +15,30 @@ import {
 from 'mdb-react-ui-kit';
 
 function Signup() {
+
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const submitForm = (e) => {
+    e.preventDefault();
+      axios
+      .post(`${SERVER_URL}/api/v1/auth/register`, formData)
+      .then((res) => {
+        console.log(res.data)
+      })
+      navigate("/");
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
   return (
     <MDBContainer fluid className='p-4 container'>
 
@@ -39,18 +67,18 @@ function Signup() {
 
               <MDBRow>
                 <MDBCol col='6'>
-                  <MDBInput wrapperClass='mb-4' label='First name' id='form1' type='text'/>
+                  <MDBInput wrapperClass='mb-4' name='firstName' label='First name' id='form1' type='text' onChange={handleChange}/>
                 </MDBCol>
 
                 <MDBCol col='6'>
-                  <MDBInput wrapperClass='mb-4' label='Last name' id='form1' type='text'/>
+                  <MDBInput wrapperClass='mb-4' name='lastName' label='Last name' id='form1' type='text' onChange={handleChange}/>
                 </MDBCol>
               </MDBRow>
 
-              <MDBInput wrapperClass='mb-4' label='Email' id='form1' type='email'/>
-              <MDBInput wrapperClass='mb-4' label='Password' id='form1' type='password'/>
+              <MDBInput wrapperClass='mb-4' name='email' label='Email' id='form1' type='email' onChange={handleChange}/>
+              <MDBInput wrapperClass='mb-4' name='password' label='Password' id='form1' type='password' onChange={handleChange}/>
 
-              <MDBBtn className='w-100 mb-4' size='md'>sign up</MDBBtn>
+              <MDBBtn className='w-100 mb-4' size='md' onClick={submitForm}>sign up</MDBBtn>
 
               <div className='text-center text-md-center mt-4 pt-2'>
                 <p className="small fw-bold mt-2 pt-1 mb-2">Already have an account? <a href="/" className="link-primary links">Login</a></p>
